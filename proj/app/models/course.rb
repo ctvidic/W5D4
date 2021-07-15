9641eca1dd2da80523902621efc99058af0e2606
Kira Porter <kierxin@gmail.com>
2021-07-15 14:29:16 -0400

    Completed associations exercise

--- proj/app/models/course.rb
+++ proj/app/models/course.rb
@@ -1,2 +1,14 @@
 class Course < ApplicationRecord
+    has_many(
+        :enrollments,
+        class_name: "Enrollment",
+        foreign_key: :course_id,
+        primary_key: :id
+    )
+
+    has_many(
+        :students,
+        through: :enrollments,
+        source: :student
+    )
 end
